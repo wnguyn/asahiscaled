758ec72882ab6c6bf83fbee63d620ef962556176
bob <tugirasles@gmail.com>
2026-07-18 22:45:07 -0500

    test supa... basic haptics

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
-
+// read file /sys/class/power_supply/macsmc_battery
 import (
 	"fmt"
-
-	tea "charm.land/bubbletea/v2"
+	"os"
+	"strconv"
+//	tea "charm.land/bubbletea/v2"
 )
 
+const url = "/sys/class/power_supply/macsmc_battery";
+
+type haptic_info struct {
+	haptic_buffer [64]int
+}
+
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("WE ARE READING ON \n FILE: {}", url)
+	for {
+		value := handle_file()
+		fmt.Println("", value)
+	}
+
+}
+
+
+
+func handle_file() int {
+	sensor_val, file_err := os.ReadFile(url)
+	// This is horrible
+	if file_err != nil {
+		return -1
+	}
+	
+	file_output, fail_int := strconv.Atoi(string(sensor_val));
+	if fail_int != nil {
+		return -1
+	}
+	return file_output;
 }

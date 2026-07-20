d75c52bdb4d32187412dc8b908c1e6dccb3067b7
bob <tugirasles@gmail.com>
2026-07-19 23:56:46 -0500

    change to idiomatic camel case NO SNAKE CASE :(

--- main.go
+++ main.go
@@ -1,39 +1,64 @@
 package main
+
 // read file /sys/class/power_supply/macsmc_battery
 import (
 	"fmt"
 	"os"
 	"strconv"
-//	tea "charm.land/bubbletea/v2"
+	"time"
+	// tea "charm.land/bubbletea/v2"
 )
 
-const url = "/sys/class/power_supply/macsmc_battery";
 
-type haptic_info struct {
-	haptic_buffer [64]int
+
+const (
+	NoFinger FingerStatus = foo
+	FingerOnPad
+	ActiveMeasure
+)
+const url = "/sys/class/power_supply/macsmc_battery"
+
+type hapticInfo struct {
+	hapticBuffer [64]int 
+	tail          int
+	enumTail int // meant to keep tabs w/ enum status
+	calcValue int 
 }
 
 func main() {
 	fmt.Println("WE ARE READING ON \n FILE: {}", url)
+
+	hapticInfo := hapticInfo{
+		hapticBuffer: []int,
+		tail:          0,
+	}
+
 	for {
-		value := handle_file()
-		fmt.Println("", value)
+		value := handleFile()
+		fillBuffer(&hapticInfo, value)
+		sortEnum()
 	}
 
 }
 
+func fillBuffer(a *hapticInfo, val int) {
+	idx := a.tail
+	a.hapticBuffer[idx] = val
+	if val != -1 {
+		a.tail++
+	}
+}
 
-
-func handle_file() int {
-	sensor_val, file_err := os.ReadFile(url)
-	// This is horrible
-	if file_err != nil {
+func handleFile() int {
+	sensorVal, fileErr := os.ReadFile(url)
+	// This is horrible holy shit
+	if fileErr != nil {
 		return -1
 	}
-	
-	file_output, fail_int := strconv.Atoi(string(sensor_val));
-	if fail_int != nil {
+
+	fileOutput, failInt := strconv.Atoi(string(sensorVal))
+	if failInt != nil {
 		return -1
 	}
-	return file_output;
+	return fileOutput
 }
---
+++ scale.go
@@ -0,0 +1,3 @@
+package main
+
+func handleFunction(gb)
